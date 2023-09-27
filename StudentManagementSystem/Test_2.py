bf4d797c3a39639ef62bd6809df85c14210ac9e4
JiaXionG628 <q526468492@gmail.com>
2023-09-27 14:22:34 +0800

    update homework2

--- StudentManagementSystem/Test_2.py
+++ StudentManagementSystem/Test_2.py
@@ -15,6 +15,7 @@ class Student(object):
 class StudentManagement(object):
 
     def __init__(self):
+        self.student_id = 1001
         self.student_list = []
 
     def print_student_info(func):
@@ -28,30 +29,35 @@ class StudentManagement(object):
         return wrapper
 
     @print_student_info
-    def addStudent(self, student):
-        print('添加后的学员信息：')
-        self.student_list.append(student)
+    def addStudent(self, student_name, student_sex):
+        if student_sex != '男' and student_sex != '女':
+            print('性别输入有误')
+            return -1
+        self.student_list.append(Student(self.student_id, student_name, student_sex))
+        self.student_id += 1
+        print('添加的学员信息：')
+        return 0
 
     @print_student_info
     def deleteStudent(self, student_id: int):
+        if not isinstance(student_id, int):
+            print("删除的学号不是数字")
+            return -1
         print("删除后的学员信息")
         for student in self.student_list:
             if student.id == student_id:
                 self.student_list.remove(student)
                 break
         else:
-            print("未找到该学员")
+            print("输入学号不存在")
+            return -1
+        return 0
 
 
 if __name__ == '__main__':
-    student_1 = Student(1001, '张三', '男')
-    student_2 = Student(1002, '莉丝', '女')
-    student_3 = Student(1003, '王武', '男')
     students = StudentManagement()
-    students.addStudent(student_1)
-    students.addStudent(student_2)
-    students.addStudent(student_3)
-    students.deleteStudent(1002)
+    students.addStudent('张三', '男')
+    students.addStudent('莉丝', '女')
+    students.addStudent('王武', '男')
+    students.deleteStudent('你好')
 
-    # for student in students.student_list:
-    #     print(student)
