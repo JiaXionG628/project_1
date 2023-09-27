2e08685ba7642bece7f0c7c844e8b58d5a7719a0
JiaXionG628 <q526468492@gmail.com>
2023-09-27 13:55:25 +0800

    update homework‘ “

--- StudentManagementSystem/Test_1.py
+++ StudentManagementSystem/Test_1.py
@@ -5,26 +5,30 @@ class Student(object):
         self.name = student_name
         self.sex = student_sex
 
+    def __repr__(self):
+        return f'学号：{self.id}, 姓名：{self.name}, 性别：{self.sex}'
+
 
 class StudentManagement(object):
 
     def __init__(self):
+        self.student_id = 1001
         self.student_list = []
 
-    def addStudent(self, student):
-        self.student_list.append(student)
+    def addStudent(self, student_name, student_sex):
+        if student_sex != '男' and student_sex != '女':
+            print('性别输入有误')
+            return -1
+        self.student_list.append(Student(self.student_id, student_name, student_sex))
+        self.student_id += 1
+        return 0
 
 
 if __name__ == '__main__':
-
-    student_1 = Student(1001, '张三', '男')
-    student_2 = Student(1002, '莉丝', '女')
-    student_3 = Student(1003, '王武', '男')
     students = StudentManagement()
-    students.addStudent(student_1)
-    students.addStudent(student_2)
-    students.addStudent(student_3)
-
+    students.addStudent('张三', '男')
+    students.addStudent('莉丝', '女')
+    students.addStudent('王武', '男')
     print('添加的学员信息：')
     for student in students.student_list:
-        print(f'学号：{student.id}, 姓名：{student.name},性别：{student.sex}')
+        print(student)
